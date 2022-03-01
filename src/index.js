c06e4fce0d379ac7fe12cf2c17592e84c1f78053
Jessica Agudelo <jessicaagudelo2606@gmail.com>
2022-02-28 22:31:59 -0500

    Keep cleaning initial files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Holi</h1>,
-  document.getElementById('root')
+  <h1>Holi Bebes!</h1>,
+  document.getElementById('app')
 );
