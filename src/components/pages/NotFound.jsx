61e6a19b2f294769d2f4c1d54038e66167efcdc0
Jessica Agudelo <jessicaagudelo2606@gmail.com>
2022-03-03 23:56:12 -0500

    Create and route NotFound page

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import DefaultLayout from "../layouts/DefaultLayout";
+
+/**
+ * Not found component.
+ 
+ * @returns{JSX.Element}
+ */
+
+export const NotFound = () => (
+    <DefaultLayout>
+        <h1>Not Found page</h1>
+    </DefaultLayout>
+);
+ 
+
+export default NotFound;
