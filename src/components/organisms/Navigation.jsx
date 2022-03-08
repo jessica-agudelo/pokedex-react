8e7475a2e159f49e97ea6bfd14a8ce5e63930cbf
Jessica Agudelo <jessicaagudelo2606@gmail.com>
2022-03-07 23:29:25 -0500

    Define NavBrand component

--- src/components/organisms/Navigation.jsx
+++ src/components/organisms/Navigation.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import NavBrand from "../../molecules/NavBrand";
 /**
  * Navigation component. 
  * @returns{JSX.Element}
  */
 
 
-export const Navigation = () =><nav></nav>;
-
+export const Navigation = () =>(
+<nav>
+   <NavBrand>
+       
+   </NavBrand>
+</nav>
+);
 export default Navigation;
---
+++ src/molecules/NavBrand.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+/**
+ * Navigation Brand component. 
+ * @returns{JSX.Element}
+ */
+
+
+export const NavBrand = () => {
+ let navigate = useNavigate();
+ const handleClick=()=>{
+     navigate('/');
+ };
+    return(
+    <div onClick={handleClick}>
+        <img src='' alt="Logo de la app" />
+            <span>POKEDEX_</span>
+    </div>
+)};
+
+export default NavBrand;
