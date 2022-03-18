b176981d9db590e68f66e1df81ef12d0d8d6e904
Jessica Agudelo <jessicaagudelo2606@gmail.com>
2022-03-17 22:13:00 -0500

    Change tex for page link

--- src/components/molecules/nav-brand/NavBrand.jsx
+++ src/components/molecules/nav-brand/NavBrand.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from 'assets/img/logo.png';
-import "./nav-brand.css";
+import "./NavBrand.css";
 /**
  * Navigation Brand component. 
  * @returns{JSX.Element}
