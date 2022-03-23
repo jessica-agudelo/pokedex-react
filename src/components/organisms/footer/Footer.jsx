64ff35216b8dc4e3211f14540fb7b3bddb14a20a
Jessica Agudelo <jessicaagudelo2606@gmail.com>
2022-03-23 00:00:30 -0500

    Define footer

--- src/components/organisms/footer/Footer.jsx
+++ src/components/organisms/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import'./Footer.css';
 /**
  * Footer component. 
  * @returns{JSX.Element}
  */
 
- const Footer = () =><footer></footer>;
+ const Footer = () => <footer className='o-footer'>&copy; BUENA MAI</footer>;
 
 export default Footer;
