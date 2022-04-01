f1190f2068291d5f4fe83a465b11cbc9adbc6554
Jessica Agudelo <jessicaagudelo2606@gmail.com>
2022-03-31 21:59:48 -0500

    fetch and show basic pokemon data

--- src/components/pages/pokemon-details/PokemonDetails.jsx
+++ src/components/pages/pokemon-details/PokemonDetails.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import DefaultLayout from 'components/templates/default-layout/DefaultLayout';
-
+import { useParams } from 'react-router-dom';
+import { fetchPokemon } from 'assets/js/pokeapi-handler';
+import PokemonView from 'components/molecules/pokemon-view/PokemonView';
 /**
- * PokemonDetails component. 
+ * Pokemon Details component. 
  * @returns{JSX.Element}
  */
- const PokemonDetails = () =>(
-    <DefaultLayout title='Detalle'>
-        <h1>Pokemon Details Page</h1>
-    </DefaultLayout>
-);
+const PokemonDetails = () =>{
+     const {name: pokemonName} = useParams();
+     const [pokemonData, setPokemonData] = useState(null);
+     useEffect(async () => {
+       setPokemonData(await fetchPokemon(pokemonName));
+     });
+   return(
+     <DefaultLayout title='Detalle'>
+      <div>
+       {pokemonData ? (
+          <PokemonView name={pokemonData.name} image= {pokemonData.image}/>
+        ) : (
+         <span>Pokemon no disponible</span> 
+        )}
+         </div>
+     </DefaultLayout>
+   );
+};
 export default PokemonDetails;
